b0b91ac03fedf756b7265c1dbc61ca3776db595d
Fluff 5 <76451731+Fluff5@users.noreply.github.com>
2024-06-27 09:32:02 +0100

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -37,7 +37,7 @@ GameManager.prototype.isGameTerminated = function () {
 GameManager.prototype.setup = function () {
   this.grid        = new Grid(this.size);
 
-  this.score       = 1;
+  this.score       = 0;
   this.over        = false;
   this.won         = false;
   this.keepPlaying = false;
